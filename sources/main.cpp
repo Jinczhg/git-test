6d6b0ace04c9a8f8f2a30ac101f5a18ccbda9e73
Yangxin Wang <wangyangxin@i.shu.edu.com>
2017-07-17 10:58:25 +0800

    change main: input test3

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-	string img0_path = "input/test4/IMG_0.png";
-	string img1_path = "input/test4/IMG_1.png";
-	string simg0_path = "input/test4/IMG_0_smooth.png";
-	string simg1_path = "input/test4/IMG_1_smooth.png";
-	//string fea_path = "input/test1/features.txt";
-	//string mat_path = "input/test1/matches.txt";
-	string out_path = "output/test4";
+	string img0_path = "input/test3/IMG_0.png";
+	string img1_path = "input/test3/IMG_1.png";
+	string simg0_path = "input/test3/IMG_0_smooth.png";
+	string simg1_path = "input/test3/IMG_1_smooth.png";
+	//string fea_path = "input/test3/features.txt";
+	//string mat_path = "input/test3/matches.txt";
+	string out_path = "output/test3";
 
 	_mkdir("output");
 	_mkdir(out_path.c_str());
